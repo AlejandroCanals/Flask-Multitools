397e6efb0477cf902ace3c5078d11bac0538d907
Alejandro Canals <129771462+iTzSRK@users.noreply.github.com>
2023-10-24 17:25:10 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-from app import app  # Importa la instancia de Flask desde la aplicación principal
+from app import app
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8501,debug=True)  # Ejecuta la aplicación en el servidor
-
+if __name__ == "__main__":
+    app.run()
