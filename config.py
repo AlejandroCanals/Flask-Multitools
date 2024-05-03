26d8f2ad69992b16f400842468f6552d5d44eb9d
Alejandro Canals <129771462+AlejandroCanals@users.noreply.github.com>
2024-05-03 17:43:54 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,5 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 # Configuración de seguridad
 SESSION_COOKIE_SECURE = True  # Establecer a True en producción para habilitar cookies seguros
 SESSION_COOKIE_HTTPONLY = True
+
+ALLOWED_HOST = 'flaskmultitools.onrender.com'
